d37a57c1073a98fe09972f087637628240e4b8a8
Austen Groener <rocketboyausten@gmail.com>
2015-02-21 10:44:24 -0500

    Finished GA08_1.py.

---
+++ GA08_1.py
@@ -0,0 +1,117 @@
+from xlrd import open_workbook
+
+from astropy.table import Table
+from astropy.io import ascii
+
+import Uncertainties as un
+import MConvert_Personal as mc
+
+import ipdb
+
+# Opening Excel File
+wb = open_workbook('/Users/groenera/Desktop/Dropbox/Private/Research/DataFiles/ObservedClusterConcsDB/cm_data.xlsx')
+
+# First sheet
+sheet_names = wb.sheet_names()
+sheet1 = wb.sheet_by_name(sheet_names[0])
+
+# Get headers and data
+headers = []
+
+clusters = sheet1.col_values(0)
+headers.append(clusters.pop(0))
+
+redshift = sheet1.col_values(1)
+headers.append(redshift.pop(0))
+
+methods = sheet1.col_values(2)
+headers.append(methods.pop(0))
+
+c200 = sheet1.col_values(3)
+headers.append(c200.pop(0))
+
+c200_plus = sheet1.col_values(4)
+headers.append(c200_plus.pop(0))
+
+c200_minus = sheet1.col_values(5)
+headers.append(c200_minus.pop(0))
+
+m200 = sheet1.col_values(6)
+headers.append(m200.pop(0))
+
+m200_plus = sheet1.col_values(7)
+headers.append(m200_plus.pop(0))
+
+m200_minus = sheet1.col_values(8)
+headers.append(m200_minus.pop(0))
+
+cvir = sheet1.col_values(9)
+headers.append(cvir.pop(0))
+
+cvir_plus = sheet1.col_values(10)
+headers.append(cvir_plus.pop(0))
+
+cvir_minus = sheet1.col_values(11)
+headers.append(cvir_minus.pop(0))
+
+mvir = sheet1.col_values(12)
+headers.append(mvir.pop(0))
+
+mvir_plus = sheet1.col_values(13)
+headers.append(mvir_plus.pop(0))
+
+mvir_minus = sheet1.col_values(14)
+headers.append(mvir_minus.pop(0))
+
+short_refs = sheet1.col_values(15)
+headers.append(short_refs.pop(0))
+
+orig_convention = sheet1.col_values(16)
+headers.append(orig_convention.pop(0))
+
+cosmology = sheet1.col_values(17)
+headers.append(cosmology.pop(0))
+
+# collect relavent data; there are no nans, TBD's, 
+# but there are asymmetric uncertainties here to deal with
+ga08_clusters = [clusters[i] for i in range(len(clusters)) if short_refs[i] == 'GA08.1']
+ga08_mvir = [mvir[i] for i in range(len(mvir)) if short_refs[i] == 'GA08.1']
+ga08_mvir_p = [mvir_plus[i] for i in range(len(mvir_plus)) if short_refs[i] == 'GA08.1']
+ga08_mvir_m = [mvir_minus[i] for i in range(len(mvir_minus)) if short_refs[i] == 'GA08.1']
+ga08_cvir = [cvir[i] for i in range(len(cvir)) if short_refs[i] == 'GA08.1']
+ga08_cvir_p = [cvir_plus[i] for i in range(len(cvir_plus)) if short_refs[i] == 'GA08.1']
+ga08_cvir_m = [cvir_minus[i] for i in range(len(cvir_minus)) if short_refs[i] == 'GA08.1']
+ga08_z = [redshift[i] for i in range(len(redshift)) if short_refs[i] == 'GA08.1']
+ga08_deltavir = [mc.DeltaFinder(0.3,0.7,redshift[i]) for i in range(len(redshift)) if short_refs[i] == 'GA08.1']
+
+# testing to make sure that data are in the expected format
+# make sure both concentration and mass measurements are there in all cases
+assert len(ga08_mvir) == len(ga08_cvir)
+# make sure both mass uncertainties are there in all cases, and are the same
+assert len(ga08_mvir_p) == len(ga08_mvir), "Mvir uncertainties appear to be missing for some clusters..."
+assert len(ga08_mvir_p) == len(ga08_mvir_m), "Mvir uncertainties do not have the same length..."
+for i in range(len(ga08_mvir)):
+    assert abs(ga08_mvir_p[i]) == abs(ga08_mvir_m[i]), "Mass uncertainties differ for cluster {}".format(ga08_clusters[i])
+# make sure both concentration uncertainties are there in all cases, and are the same
+assert len(ga08_cvir_p) == len(ga08_cvir), "cvir uncertainties appear to be missing for some clusters..."
+assert len(ga08_cvir_p) == len(ga08_cvir_m), "cvir uncertainties do not have the same length..."
+for i in range(len(ga08_mvir)):
+    assert abs(ga08_cvir_p[i]) == abs(ga08_cvir_m[i]), "Conc. uncertainties differ for cluster {}".format(ga08_clusters[i])
+# checking redshifts and deltavirs
+assert len(ga08_z) == len(ga08_mvir)
+assert len(ga08_deltavir) == len(ga08_mvir)
+
+# time to convert
+# propagate uncertainties by conserving fractional uncertainties
+# decided to do this for all CO09 clusters because one or more had asymmetric uncertainties
+delta_new = 200 # constant for all clusters
+accuracy = 2 # number of places after the decimal to round to
+ga08_m200 = [round(mc.Mconvert(ga08_mvir[i],ga08_deltavir[i],delta_new,ga08_cvir[i]),accuracy) for i in range(len(ga08_mvir))]
+ga08_m200_err = [round(un.propagate_mass_uncertainty_notindependent(ga08_mvir[i],ga08_mvir_p[i],ga08_cvir[i],ga08_cvir_p[i],ga08_deltavir[i],delta_new),accuracy) for i in range(len(ga08_mvir))]
+ga08_c200 = [round(mc.Cconvert(ga08_mvir[i],ga08_deltavir[i],delta_new,ga08_cvir[i]),accuracy) for i in range(len(ga08_mvir))]
+ga08_c200_err = [round(un.propagate_conc_uncertainty(ga08_mvir[i],ga08_mvir_p[i],ga08_cvir[i],ga08_cvir_p[i],ga08_deltavir[i],delta_new),accuracy) for i in range(len(ga08_mvir))]
+
+for i in range(len(ga08_mvir)):
+    print "{}, {}, {}, {}, {}, -{}, {}, {}, -{}, {}, {}, {}, {}, {}, {}".format(i+1,ga08_clusters[i],ga08_z[i],ga08_c200[i],ga08_c200_err[i],ga08_c200_err[i],ga08_m200[i],ga08_m200_err[i],ga08_m200_err[i],ga08_cvir[i],ga08_cvir_p[i],ga08_cvir_m[i],ga08_mvir[i],ga08_mvir_p[i],ga08_mvir_m[i])
+
+
