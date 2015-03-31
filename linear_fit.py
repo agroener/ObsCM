24f44c983d33b8ff50d671f8ce13b1ee4d2332ae
Austen Groener <amg338@drexel.edu>
2015-03-31 16:03:46 -0400

    Did a heck of a lot of work within linear_fit.py. Extended functionality to include bootstrapping of parameters.

--- linear_fit.py
+++ linear_fit.py
@@ -51,14 +51,29 @@ def second_partial_derivative_mixed(func, var=[5,6], point=()):
         print("Need to implement partial derivative...")
 
 
-def startup(fh=None):
-    if fh is not None:
-        x,y,sigx,sigy=loadtxt(fh,delimiter=",",usecols=(0,1,2,3),unpack=True,comments='#')
+def startup(fname=None):
+    if fname is not None:
+        x,y,sigx,sigy=loadtxt(fname,delimiter=",",usecols=(0,1,2,3),unpack=True,comments='#')
         return x,y,sigx,sigy
     else:
         print("Filename must be specified...")
         return
 
+def startup_bootstrap(fname=None):
+    # first load in all of the data
+    if fname is not None:
+        x,y,sigx,sigy=loadtxt(fname,delimiter=",",usecols=(0,1,2,3),unpack=True,comments='#')
+    else:
+        print("Filename must be specified...")
+        return
+    # now sample with replacement
+    x_bs = np.random.choice(x,replace=True,size=len(x))
+    bs_indices = [np.where(x == x_bs[i])[0][0] for i in range(len(x_bs))]
+    y_bs = np.array([y[i] for i in bs_indices])
+    sigx_bs = np.array([sigx[i] for i in bs_indices])
+    sigy_bs = np.array([sigy[i] for i in bs_indices])
+    return x_bs,y_bs,sigx_bs,sigy_bs
+        
 def steepest_decent(x,y,sigx,sigy,sig,m,b,N,alpha=0.8,tol=1e-6):
        
     converged = False
@@ -201,9 +216,161 @@ def fit(method='X-ray', plot=False, savefig=False):
         plt.show()
 
     return m2,sigm,b2,sigb,sig,(a1,b1,theta)
-        
+
+def fit_bootstrap(method='X-ray', witherrors=True, nsamples=100):
+    # Load the data from file
+    if method in ['X-ray','x-ray','xray']:
+        pl_col = 'green'
+        method_title = 'X-ray'
+        filename='{}_data.txt'.format('X-ray')
+    elif method in ['WL','wl']:
+        pl_col = 'purple'
+        method_title = 'WL'
+        filename='{}_data.txt'.format('WL')
+    elif method in ['SL','sl']:
+        pl_col = 'red'
+        method_title = 'SL'
+        filename='{}_data.txt'.format('SL')
+    elif method in ['WL+SL','wl+sl']:
+        pl_col = 'black'
+        method_title = 'WL+SL'
+        filename='{}_data.txt'.format('WL+SL')
+    elif method in ['CM','cm']:
+        pl_col = 'blue'
+        method_title = 'CM'
+        filename='{}_data.txt'.format('CM')
+    elif method in ['LOSVD','losvd']:
+        pl_col = 'orange'
+        method_title = 'LOSVD'
+        filename='{}_data.txt'.format('LOSVD')
+    else:
+        print("Method is undefined...")
+        return
+
+    if witherrors is False:
+        m1_list, b1_list, sig_list = ([],[],[])
+        for i in range(nsamples):
+            x_bs,y_bs,sigx_bs,sigy_bs = startup_bootstrap(fname=filename)
+            N=len(x_bs)
+            Sxy=sum(x_bs*y_bs)
+            Sx=sum(x_bs)
+            Sy=sum(y_bs)
+            Sxx=sum(x_bs*x_bs)
+            m1=(N*Sxy-Sx*Sy)/(N*Sxx-Sx*Sx)
+            b1=(-Sx*Sxy+Sxx*Sy)/(N*Sxx-Sx*Sx)
+            sig=sqrt(std(y_bs-(m1*x_bs+b1))**2-mean(sigy_bs**2))
+            m1_list.append(m1)
+            b1_list.append(b1)
+            sig_list.append(sig)
+        return m1_list, b1_list, sig_list
+
+    if witherrors is True:
+        m2_list, b2_list = ([],[])
+        for i in range(nsamples):
+            x_bs,y_bs,sigx_bs,sigy_bs = startup_bootstrap(fname=filename)
+            N=len(x_bs)
+            Sxy=sum(x_bs*y_bs)
+            Sx=sum(x_bs)
+            Sy=sum(y_bs)
+            Sxx=sum(x_bs*x_bs)
+            m1=(N*Sxy-Sx*Sy)/(N*Sxx-Sx*Sx)
+            b1=(-Sx*Sxy+Sxx*Sy)/(N*Sxx-Sx*Sx)
+            sig=sqrt(std(y_bs-(m1*x_bs+b1))**2-mean(sigy_bs**2))
+            m2,b2 = steepest_decent(x_bs,y_bs,sigx_bs,sigy_bs,sig,m1,b1,N,alpha=0.75,tol=1.e-6)
+            m2_list.append(m2)
+            b2_list.append(b2)
+        return m2_list, b2_list
+
+def do_bootstrap(method = 'X-ray', nsamples = 1000, witherrors = False, showplots=False, savepath=None):
+    if method in ['X-ray','x-ray','xray']:
+        pl_col = 'green'
+        method_title = 'X-ray'
+        filename='{}_data.txt'.format('X-ray')
+    elif method in ['WL','wl']:
+        pl_col = 'purple'
+        method_title = 'WL'
+        filename='{}_data.txt'.format('WL')
+    elif method in ['SL','sl']:
+        pl_col = 'red'
+        method_title = 'SL'
+        filename='{}_data.txt'.format('SL')
+    elif method in ['WL+SL','wl+sl']:
+        pl_col = 'black'
+        method_title = 'WL+SL'
+        filename='{}_data.txt'.format('WL+SL')
+    elif method in ['CM','cm']:
+        pl_col = 'blue'
+        method_title = 'CM'
+        filename='{}_data.txt'.format('CM')
+    elif method in ['LOSVD','losvd']:
+        pl_col = 'orange'
+        method_title = 'LOSVD'
+        filename='{}_data.txt'.format('LOSVD')
+    else:
+        print("Method is undefined...")
+        return
+
+    if savepath is None:
+        path = ''
+    else:
+        path = savepath
+
+    if witherrors is False:
+        err_str = 'withouterr'
+        m, b, sig = fit_bootstrap(method=method, witherrors=witherrors, nsamples=nsamples)
+        m = [m[i] for i in range(len(m)) if not np.isnan(m[i])]
+        b = [b[i] for i in range(len(b)) if not np.isnan(b[i])]
+        sig = [sig[i] for i in range(len(sig)) if not np.isnan(sig[i])]
+
+    elif witherrors is True:
+        err_str = 'witherr'
+        m, b = fit_bootstrap(method=method, witherrors=witherrors, nsamples=nsamples)
+        m = [m[i] for i in range(len(m)) if not np.isnan(m[i])]
+        b = [b[i] for i in range(len(b)) if not np.isnan(b[i])]
+
+
+
+    plt.figure()
+    plt.hist(m,bins=30,color=pl_col,histtype='stepfilled',alpha=0.5)
+    plt.title('Number of Bootstrap Samples: {}'.format(nsamples))
+    plt.xlabel(r'$\mathrm{m}$',fontsize=18)
+    plt.axvline(x=np.average(m),color='black',linewidth=3)
+    plt.axvline(x=np.average(m)+np.std(m),linestyle='--',color='black',linewidth=3)
+    plt.axvline(x=np.average(m)-np.std(m),linestyle='--',color='black',linewidth=3)
+    plt.text(np.average(m)-3*np.std(m),35,"mean: {:.4f}".format(np.average(m)))
+    plt.text(np.average(m)-3*np.std(m),25,"std.: {:.4f}".format(np.std(m)))
+    plt.savefig(path+"{}_m_bootstrap_{}.png".format(method_title,err_str))
+    
+    plt.figure()
+    plt.hist(b,bins=30,color=pl_col,histtype='stepfilled',alpha=0.5)
+    plt.title('Number of Bootstrap Samples: {}'.format(nsamples))
+    plt.xlabel(r'$\mathrm{b}$',fontsize=18)
+    plt.axvline(x=np.average(b),color='black',linewidth=3)
+    plt.axvline(x=np.average(b)+np.std(b),linestyle='--',color='black',linewidth=3)
+    plt.axvline(x=np.average(b)-np.std(b),linestyle='--',color='black',linewidth=3)
+    plt.text(np.average(b)-3*np.std(b),35,"mean: {:.4f}".format(np.average(b)))
+    plt.text(np.average(b)-3*np.std(b),25,"std.: {:.4f}".format(np.std(b)))
+    plt.savefig(path+"{}_b_bootstrap_{}.png".format(method_title,err_str))
+
+    plt.figure()
+    plt.hist(sig,bins=30,color=pl_col,histtype='stepfilled',alpha=0.5)
+    plt.title('Number of Bootstrap Samples: {}'.format(nsamples))
+    plt.xlabel(r'$\mathrm{\sigma}$',fontsize=18)
+    plt.axvline(x=np.average(sig),color='black',linewidth=3)
+    plt.axvline(x=np.average(sig)+np.std(sig),linestyle='--',color='black',linewidth=3)
+    plt.axvline(x=np.average(sig)-np.std(sig),linestyle='--',color='black',linewidth=3)
+    plt.text(np.average(sig)-3*np.std(sig),35,"mean: {:.4f}".format(np.average(sig)))
+    plt.text(np.average(sig)-3*np.std(sig),25,"std.: {:.4f}".format(np.std(sig)))
+    plt.savefig(path+"{}_sig_bootstrap_{}.png".format(method_title,err_str))
+
+    if showplots is True:
+        plt.show()
+
+    
 if __name__ == "__main__":
 
+    # Plotting the fit (and uncertainty regions) over the data
+    '''
     m_xray,sigm_xray,b_xray,sigb_xray,sig_xray,(a1_xray,b1_xray,theta_xray)=fit(method='xray')#,plot=True, savefig=True)
     m_wl,sigm_wl,b_wl,sigb_wl,sig_wl,(a1_wl,b1_wl,theta_wl)=fit(method='wl')#,plot=True, savefig=True)
     m_sl,sigm_sl,b_sl,sigb_sl,sig_sl,(a1_sl,b1_sl,theta_sl)=fit(method='sl')#,plot=True, savefig=True)
@@ -230,7 +397,8 @@ if __name__ == "__main__":
     plt.xlabel(r'$\mathrm{\log \, M_{vir}/M_{\odot}}$',fontsize=20)
     plt.ylabel(r'$\mathrm{\log \, c_{vir} (1+z) }$',fontsize=20)
     plt.savefig('ObsCM_LinearModel.png')
-
+    '''
+    
     # Plotting all of the error ellipses on the same plot
     # Not that informative of a plot...
     '''
@@ -302,3 +470,20 @@ if __name__ == "__main__":
 
     plt.show()
     '''
+
+    # Performing bootstrap
+    tmp_path = '/Users/groenera/Desktop/Dropbox/Private/Research/GroupMeetings/Meeting#59/'
+    #without error first
+    #do_bootstrap(method='X-ray',witherrors=False, savepath=tmp_path)
+    #do_bootstrap(method='CM',witherrors=False, savepath=tmp_path)
+    #do_bootstrap(method='WL',witherrors=False, savepath=tmp_path)
+    #do_bootstrap(method='SL',witherrors=False, savepath=tmp_path)
+    #do_bootstrap(method='WL+SL',witherrors=False, savepath=tmp_path)
+    #do_bootstrap(method='LOSVD',witherrors=False, savepath=tmp_path)
+    #with error next
+    do_bootstrap(method='X-ray',witherrors=True,savepath=tmp_path,nsamples=100)
+    do_bootstrap(method='CM',witherrors=True,savepath=tmp_path,nsamples=100)
+    do_bootstrap(method='WL',witherrors=True,savepath=tmp_path,nsamples=100)
+    do_bootstrap(method='SL',witherrors=True,savepath=tmp_path,nsamples=100)
+    do_bootstrap(method='WL+SL',witherrors=True,savepath=tmp_path,nsamples=100)
+    do_bootstrap(method='LOSVD',witherrors=True,savepath=tmp_path,nsamples=100)
