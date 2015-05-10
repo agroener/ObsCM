093bfff7379b497f9c29240cfa9dfba07b6247e4
Austen Groener <rocketboyausten@gmail.com>
2015-05-10 13:50:33 -0400

    Began the process of comparing all simulation relations in SimulationRelations.py.

---
+++ SimulationRelations.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Simulation relation functions
+def bullock():
+    return
+def prada():
+    return
+def zhao():
+    return
+def ludlow():
+    return
+def dutton():
+    return
+def battacharya():
+    return
+def correa():
+    return
+def diemer():
+    return
+def vandenbosch():
+    return
+
+# Function for finding Groener and Goldberg data
+def startup_sims():
+    try:
+        raw_data = read('/Users/groenera/Desktop/Dropbox/Private/Research/GroupMeetings/Meeting#60/concs_m200s_data.txt',delimiter=',',guess=False)
+    except:
+        print("Cannot find datafile...")
+        return
+
+    raw_concs = raw_data['c200']
+    raw_fof = raw_data['mfof']
+    raw_masses = raw_data['m200']
+
+    high_indices = [i for i in range(len(raw_fof)) if raw_fof[i] > 1.2e14]
+    h_concs = [raw_concs[i] for i in range(len(raw_concs)) if i in high_indices]
+    h_masses = [raw_masses[i] for i in range(len(raw_masses)) if i in high_indices]
+
+    low_indices = [i for i in range(len(raw_masses)) if raw_fof[i] < 2.6e13]
+    l_concs = [raw_concs[i] for i in range(len(raw_concs)) if i in low_indices]
+    l_masses = [raw_masses[i] for i in range(len(raw_masses)) if i in low_indices]
+
+    med_indices = [i for i in range(len(raw_masses)) if i not in high_indices and i not in low_indices]
+    m_concs = [raw_concs[i] for i in range(len(raw_concs)) if i in med_indices]
+    m_masses = [raw_masses[i] for i in range(len(raw_masses)) if i in med_indices]
+
+    return l_concs,l_masses,m_concs,m_masses,h_concs,h_masses
+
+
+if __name__ == "__main__":
+    
+    # Get simulation results from Groener & Goldberg 2014, first
+    l_concs,l_masses,m_concs,m_masses,h_concs,h_masses = startup_sims()
+
+    # Call various simulation relations here
